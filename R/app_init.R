b9855e9c2b5d72c881077f6325ecec7aac93e035
flashl1ght <krzysztof.warzecha@protonmail.com>
2022-11-12 18:52:13 +0100

    added app starting function

---
+++ R/app_init.R
@@ -0,0 +1,27 @@
+#' Get shiny server object
+#'
+get_shiny_server <- function() {
+  return(server)
+}
+
+#' Wrapper for shinyApp function to start the dashboard
+#' 
+#' @export start_app
+#' 
+#' @examples
+#' \donotrun{
+#' start_app()
+#' }
+#' 
+#' @importFrom shiny shinyApp
+#' 
+start_app <- function() {
+  server <- get_shiny_server()
+  shiny::shinyApp(
+    ui = ui,
+    server = server,
+    options = list(
+      launch.browser = TRUE
+    )
+  )
+}
