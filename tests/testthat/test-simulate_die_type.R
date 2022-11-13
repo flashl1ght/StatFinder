5c214751b17e1e6e295e8690269a461fc4b78028
flashl1ght <krzysztof.warzecha@protonmail.com>
2022-11-13 01:46:01 +0100

    formatting changes in unit tests

--- tests/testthat/test-simulate_die_type.R
+++ tests/testthat/test-simulate_die_type.R
@@ -1,46 +1,43 @@
 test_that("simulate_die_type simulates a die roll", {
-  
   # set params
   no_dice <- 1
   die_type <- "d8"
   no_sims <- 100
   die_sides <- 8
-  
+
   # set seed and simulate using simulate_die_type
   set.seed(1)
   simulate_roll <- simulate_die_type(no_dice, die_type, no_sims)
-  
+
   # reset seed and simulate manually
   set.seed(1)
   dice_roll <- purrr::rdunif(no_sims, die_sides, 1)
-  
+
   expect_equal(simulate_roll, dice_roll)
 })
 
 test_that("simulate_die_type simulates a number of dice roll", {
-  
   # set params
   no_dice <- 3
   die_type <- "d20"
   no_sims <- 100
   die_sides <- 20
-  
+
   # set seed and simulate using simulate_die_type
   set.seed(1)
   simulate_roll <- simulate_die_type(no_dice, die_type, no_sims)
-  
+
   # reset seed and simulate manually
   set.seed(1)
   dice_roll_1 <- purrr::rdunif(no_sims, die_sides, 1)
   dice_roll_2 <- purrr::rdunif(no_sims, die_sides, 1)
   dice_roll_3 <- purrr::rdunif(no_sims, die_sides, 1)
   dice_roll <- dice_roll_1 + dice_roll_2 + dice_roll_3
-  
+
   expect_equal(simulate_roll, dice_roll)
 })
 
 test_that("simulate_die_type returns zero for invalid die type", {
-  
   # set params
   no_dice <- 3
   die_type <- "d9"
@@ -49,29 +46,27 @@ test_that("simulate_die_type returns zero for invalid die type", {
   # set seed and simulate using simulate_die_type
   set.seed(1)
   simulate_roll <- simulate_die_type(no_dice, die_type, no_sims)
-  
+
   # reset seed and simulate manually
   set.seed(1)
   dice_roll <- 0
-  
+
   expect_equal(simulate_roll, dice_roll)
 })
 
 test_that("simulate_die_type returns zero for invalid sims number", {
-  
   # set params
   no_dice <- 3
   die_type <- "d4"
   no_sims <- "aa"
-  
+
   # set seed and simulate using simulate_die_type
   set.seed(1)
   simulate_roll <- simulate_die_type(no_dice, die_type, no_sims)
-  
+
   # reset seed and simulate manually
   set.seed(1)
   dice_roll <- 0
-  
+
   expect_equal(simulate_roll, dice_roll)
 })
-
--- tests/testthat/test-simulate_iid_discrete_sum.R
+++ tests/testthat/test-simulate_iid_discrete_sum.R
@@ -1,77 +1,73 @@
 test_that("simulate_iid_discrete_sum simulates single die roll", {
-  
   # setting params
-  no_variables = 1
-  max = 6
-  no_sims = 100
-  min = 1
-  
+  no_variables <- 1
+  max <- 6
+  no_sims <- 100
+  min <- 1
+
   # set seed and simulate using simulate_iid_discrete_sum
   set.seed(1)
   simulate_roll <- simulate_iid_discrete_sum(no_variables, max, no_sims, min)
-  
-  # reset seed and simulate manually 
+
+  # reset seed and simulate manually
   set.seed(1)
   dice_roll <- purrr::rdunif(no_sims, max, min)
-  
+
   expect_equal(simulate_roll, dice_roll)
 })
 
 test_that("simulate_iid_discrete_sum simulates a number of dice roll", {
-  
   # setting params
-  no_variables = 2
-  max = 10
-  no_sims = 100
-  min = 1
-  
+  no_variables <- 2
+  max <- 10
+  no_sims <- 100
+  min <- 1
+
   # set seed and simulate using simulate_iid_discrete_sum
   set.seed(1)
   simulate_roll <- simulate_iid_discrete_sum(no_variables, max, no_sims, min)
-  
+
   # reset seed and simulate manually
   set.seed(1)
   dice_roll_1 <- purrr::rdunif(no_sims, max, min)
   dice_roll_2 <- purrr::rdunif(no_sims, max, min)
   dice_roll <- dice_roll_1 + dice_roll_2
-  
+
   expect_equal(simulate_roll, dice_roll)
 })
 
 test_that("simulate_iid_discrete_sum return zeros for invalid dice number", {
-  
   # setting params
-  no_variables = -1
-  max = 10
-  no_sims = 100
-  min = 1
-  
+  no_variables <- -1
+  max <- 10
+  no_sims <- 100
+  min <- 1
+
   # set seed and simulate using simulate_iid_discrete_sum
   set.seed(1)
   simulate_roll <- simulate_iid_discrete_sum(no_variables, max, no_sims, min)
-  
+
   # reset seed
   set.seed(1)
   dice_roll <- rep_len(0, no_sims)
-  
+
   expect_equal(simulate_roll, dice_roll)
 })
 
 test_that("simulate_iid_discrete_sum return zero for invalid sims number", {
-  
   # setting params
-  no_variables = 2
-  max = 10
-  no_sims = -1
-  min = 1
-  
+  no_variables <- 2
+  max <- 10
+  no_sims <- -1
+  min <- 1
+
   # set seed and simulate using simulate_iid_discrete_sum
   set.seed(1)
   simulate_roll <- simulate_iid_discrete_sum(no_variables, max, no_sims, min)
-  
+
   # reset seed
   set.seed(1)
   dice_roll <- 0
-  
+
   expect_equal(simulate_roll, dice_roll)
 })
