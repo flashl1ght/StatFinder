98f0bd6561198adce05f7bd20e61d236e52e3258
flashl1ght <krzysztof.warzecha@protonmail.com>
2022-11-27 00:01:51 +0100

    adding new param for create_stats_table

--- R/global.R
+++ R/global.R
@@ -5,10 +5,12 @@ global <- new.env()
 global$dice_names <- c("d4", "d6", "d8", "d10", "d12", "d20")
 global$dice_no_sides <- list(4, 6, 8, 10, 12, 20)
 global$dice_avg_value <- (1 + unlist(global$dice_no_sides)) / 2
+global$dice_stddev_values <- sqrt(unlist(global$dice_no_sides)^2 - 1) /
+  (2 * sqrt(3))
 names(global$dice_no_sides) <- global$dice_names
 
 # sum_stats_tab params
 global$sum_stats_default_sim_number <- 1000
 global$sum_stats_header <- "Roll for damage"
-global$sum_stats_description <- 
+global$sum_stats_description <-
   "Select a number of dice and see what their sum distribution looks like"
